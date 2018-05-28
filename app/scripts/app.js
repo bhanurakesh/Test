4f3be0d8079dc230521ce5b689f56f805c6abf97
bhanurakesh <bhanurekha.vanam@gmail.com>
2018-05-28 16:33:06 +0530

    Intial changes

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name movieSearchApp
+ * @description
+ * # movieSearchApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('movieSearchApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name movieSearchApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the movieSearchApp
+ */
+angular.module('movieSearchApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
